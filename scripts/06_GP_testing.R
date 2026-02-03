1a30c101f0f748218ac299a38b13a345e65ad462
Nicolas Lara <nalara@ncsu.edu>
2026-02-03 10:56:34 -0600

    Updated GP for server running

--- scripts/06_GP_testing.R
+++ scripts/06_GP_testing.R
@@ -3,7 +3,9 @@ library(sommer)
 library(glue)
 library(dplyr)
 
-dir="c:/Users/nalara/Documents/GitHub/SE_SRWW_PHG"
+#dir="c:/Users/nalara/Documents/GitHub/SE_SRWW_PHG"
+GRM_dir="/90daydata/guedira_seq_map/nico2/pangenome_multichrom/rPHG/"
+BLUE_dir="/project/guedira_seq_map/nico/pangenome/"
 
 calculate_prediction_accuracy <- function(phenotype_df, relationship_matrix, seed = 12345, withold_percent = 0.2) {
   set.seed(seed)
@@ -26,14 +28,14 @@ families <- c("UX1989", "UX1991", "UX1992", "UX1993", "UX1994",
 # seeds <- c(45174796)
 
 accuracies <- data.frame()
-drop_one_family <- data.frame()
+#drop_one_family <- data.frame()
 for (mat in c("GRM", "Additive_GRM", "HRM")) {
   ##read in and format Kinship matrix
-  K <- read.delim(glue("{dir}/output/{mat}.csv"), sep=",", row.names = 1)
+  K <- read.delim(glue("{GRM_dir}/{mat}.csv"), sep=",", row.names = 1)
   colnames(K) <- rownames(K)
   K <- as.matrix(K)
   ###read in phenotypic values
-  blues <- read.delim(glue("{dir}/data/blues.csv"), sep=",")
+  blues <- read.delim(glue("{BLUE_dir}/data/blues.csv"), sep=",")
   row.names(blues) <- blues$Entry
   ##line up datasets
   blues <- blues[blues$Entry %in% row.names(K),]
@@ -52,26 +54,26 @@ for (mat in c("GRM", "Additive_GRM", "HRM")) {
         accuracies <- rbind(accuracies, df)
       }
     }
-    for (fam in families) {
-      y.trn <- phenotype_df
-      vv <- grep(fam, rownames(phenotype_df), value=T)
-      y.trn[vv,"pheno"] <- NA
-      ##run prediction model
-      ans <- mmes(pheno~1,
-                  random=~vsm(ism(Entry),Gu=K), 
-                  rcov=~units,nIters=10,
-                  data=y.trn, verbose = FALSE) # kinship based
-      pred_acc <- cor(ans$u[vv,] ,phenotype_df[vv,"pheno"], use="complete")
-      df <- data.frame(trait = trait, 
-                       family_withhheld = fam, 
-                       prediction_accuracy = pred_acc)
-      drop_one_family <- rbind(drop_one_family, df)
-    }
+    #for (fam in families) {
+    #  y.trn <- phenotype_df
+    #  vv <- grep(fam, rownames(phenotype_df), value=T)
+    #  y.trn[vv,"pheno"] <- NA
+    #  ##run prediction model
+    #  ans <- mmes(pheno~1,
+    #              random=~vsm(ism(Entry),Gu=K), 
+    #              rcov=~units,nIters=10,
+    #              data=y.trn, verbose = FALSE) # kinship based
+    #  pred_acc <- cor(ans$u[vv,] ,phenotype_df[vv,"pheno"], use="complete")
+    #  df <- data.frame(trait = trait, 
+    #                   family_withhheld = fam, 
+    #                   prediction_accuracy = pred_acc)
+    #  drop_one_family <- rbind(drop_one_family, df)
+    #}
   }
 }
 
-write.csv(accuracies, glue("{dir}/output/genomic_prediction_accuracies.csv"), row.names=F)
-write.csv(drop_one_family, glue("{dir}/output/genomic_prediction_accuracies_familywise.csv"), row.names=F)
+write.csv(accuracies, glue("{GRM_dir}/genomic_prediction_accuracies.csv"), row.names=F)
+#write.csv(drop_one_family, glue("{dir}/output/genomic_prediction_accuracies_familywise.csv"), row.names=F)
 
 # pirateplot(prediction_accuracy ~ percent_witheld + trait, data=accuracies)
 
