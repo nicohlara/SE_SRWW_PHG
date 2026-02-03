d72d5f537dda38df65dc55697ad2e0f7a0363726
Nico <nicohlara@gmail.com>
2026-02-03 11:25:38 -0500

    Added GP prediction script and figures

--- scripts/05_SNP_GRM.R
+++ scripts/05_SNP_GRM.R
@@ -1,6 +1,7 @@
 
 library(gaston)
-library(popkin)
+# library(popkin)
+library(sommer)
 library(glue)
 
 dir="c:/Users/nalara/Documents/GitHub/SE_SRWW_PHG"
@@ -11,4 +12,7 @@ genotype <- read.bed.matrix(glue("{geno_dir}/data/SunRILs_imp_filtmerge"))
 
 X <- t(as.matrix(genotype))
 SNP_GRM <- popkin(X)
+SNP_GRM2 <- A.mat(t(X))
 write.csv(SNP_GRM, glue("{dir}/output/GRM.csv"), quote = F)
+write.csv(SNP_GRM2, glue("{dir}/output/Additive_GRM.csv"), quote = F)
+
